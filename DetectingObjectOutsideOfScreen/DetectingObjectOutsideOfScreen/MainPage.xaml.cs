fcb225b63065bdbd59ee07da90bf2ad95c2be2bb
Damien Delaire <delairedd@gmail.com>
2014-11-13 09:55:17 -0800

    First checkin

---
+++ DetectingObjectOutsideOfScreen/DetectingObjectOutsideOfScreen/MainPage.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace DetectingObjectOutsideOfScreen
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+
+        private void ScrollViewer_OnViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
+        {
+            //checking to see if the object still visible in the screen
+            ShowMiniPlayer(sender);
+        }
+
+       
+        private void ShowMiniPlayer(object sender)
+        {
+		    //Getting scrollViewer information
+            var scrollViewer = (FrameworkElement)sender;
+
+            //checking to see if the object that i want to track 
+			//is still in the screen of the user
+
+			//const to set and offset, this means that when we only see the 200px
+			//of the object, we will see the other object, if we want to see check 
+			//when the object is not on the screen then set the offset to 0
+            const int offsetObjectBy = 200;
+            if (IsObjectHidden(ObjectThatIAmTracking, scrollViewer, offsetObjectBy))
+            {
+                ObjectThatIWhatToSee.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                ObjectThatIWhatToSee.Visibility = Visibility.Collapsed;
+            }
+        }
+
+       
+		//the object of this methode is to create 2 rectangles, from our two objects and see they interset or not
+        protected bool IsObjectHidden(FrameworkElement child, FrameworkElement scrollViewer, int Offset)
+        {
+		   //Getting the information related to the scrollViewer
+            GeneralTransform childTransform = child.TransformToVisual(scrollViewer);
+			//creating out first rectangle using the object that i wish to ttrack
+            Rect childRectangle = childTransform.TransformBounds(new Rect(new Point(0, 0), child.RenderSize));
+            //creating out second rectangle  using the scrollViewer
+			Rect ownerRectangle = new Rect(new Point(0, 0), scrollViewer.RenderSize);
+			//Testing
+            bool isInterset = IntersectsWith(childRectangle, ownerRectangle, Offset);
+            return !isInterset;
+        }
+
+        public static bool IntersectsWith(Rect rect, Rect other, int offset)
+        {
+            // Test for separating axis on the two rectangles
+            if (other.Bottom < rect.Top || other.Right < rect.Left
+             || other.Top > rect.Bottom || other.Left > (rect.Right - offset))
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+    }
+}
--- README.md
+++ README.md
@@ -2,6 +2,38 @@ How to detect if an object is outside of the screen for Windows Store Apps (C#/X
 =========================
 
 
+How to detect if an object is intersecting with another object
+
+This is a tutorial on how to implement a methode that allows us to see if you have 2 objects that are intersecting with one another.
+I mainly use this to see if an object is outside of the screen for the user and thus need to show him something else when that object is not visible anymore.
+
+If you don't want to have to look at the code here is the important part, these two methodes IsObjectHidden and IntersectsWith.
+
+//the object of this methode is to create 2 rectangles, from our two objects and see they intersect or not
+protected bool IsObjectHidden(FrameworkElement child, FrameworkElement scrollViewer, int Offset)
+{
+   //Getting the information related to the scrollViewer
+	GeneralTransform childTransform = child.TransformToVisual(scrollViewer);
+	//creating out first rectangle using the object that i wish to ttrack
+	Rect childRectangle = childTransform.TransformBounds(new Rect(new Point(0, 0), child.RenderSize));
+	//creating out second rectangle  using the scrollViewer
+	Rect ownerRectangle = new Rect(new Point(0, 0), scrollViewer.RenderSize);
+	//Testing
+	bool isInterset = IntersectsWith(childRectangle, ownerRectangle, Offset);
+	return !isInterset;
+}
+
+public static bool IntersectsWith(Rect rect, Rect other, int offset)
+{
+	// Test for separating axis on the two rectangles
+	if (other.Bottom < rect.Top || other.Right < rect.Left
+	 || other.Top > rect.Bottom || other.Left > (rect.Right - offset))
+	{
+		return false;
+	}
+	return true;
+}
+		
 
 License
 
